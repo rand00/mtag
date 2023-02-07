cf6983fd39e5501d575480233a7cf7186e7b7081
rand00 <oth.rand@gmail.com>
2023-02-08 00:52:53 +0100

    bin/main: Lots of todo-notes for more features

--- bin/main.ml
+++ bin/main.ml
@@ -222,9 +222,50 @@ mtag tags-intersection <paths..>
 let print_usage () = print_endline usage
 
 (*goto later; extending interface;
-  * mtag mv tag1 foo/tag2
+  * mtag replace-paths dir0 dir1
+    * for moving files (/fixing symlinks), including directories -
+      * user supplies CWD-relative paths which are mapped relative to mtag-root
+    * @note on impl; make lib-API nice to use for `mtag daemon`
+  * mtag daemon
+    * < daemon-mode that listens to filesystem to map symlinks automatically
+      * > becomes a filesystem tagger for dynamic filesystems!
+      * @nice; should be easy to implement
+    * using: inotifywait (can listen to 'move' + 'delete')
+    * perf-note: a single 'mv' can become _very_ expensive
+      * all tags need to be looked at
+        * all files that match need to get symlinks mapped
+          * e.g. all children of some major dir
+      * @idea; could keep whole _mtags in memory and listen to FS changes to this too
+        * fixes:
+          * symlinks matching moved file are quickly found
+          * all tags are cached in mem, so no major FS access 
+  * rename title (after prev features): "the universal file tagger"
+    * don't mention 'media' to open up peoples minds to what can be tagged
+    * 'universal' => any kind of file
+    * @idea; rename binary?
+      * e.g. `mtag` => `utag` 
+        * I like `mtag`.. feels more unique, and like hint to original purpose
+          related to media
+      * e.g. `mtag` => `ftag` 
+  * add more interesting/complex examples to docs
+    * interesting combinations of mtag
+    * xargs + other programs ++
+  * add section to README.md about other related tools
+  * mtag query 'score/{<=4}'
+    * where
+      * `{}` encapsulates expressions on strings
+      * `<=` means LTE for strings (which works for numbers too)
+  * mtag query --long ..
+    * output a human-readable + script-parseable view of files and all their tags
+    * (inspired by tmsu.org)
   * mtag list-unique 
     * < see code:projects:art:niseq:subtodos:20200713 how to use n tag tagging
+      * .. some sort of analysis for uniqueness of queried files
+  * mtag export <query> <dir>
+    * exports all resulting files with original filenames + unique-postfix to <dir>
+      as symlinks (inspired by tmsu)
+      * creates the dir if doesn't exist
+      * asks to override dir if does exist with files inside
 *)
 
 let main () =
